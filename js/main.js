82ac35f197df2803a984409cdabe3abe4472dbae
KodokuOdius <alanfoxincoolctranica@gmail.com>
2022-06-19 05:03:51 +0300

    end home page - need fast create 4 pages

--- js/main.js
+++ js/main.js
@@ -9,3 +9,14 @@ $(function() {
        nextArrow: `<button type="button" class="slick-next">&rsaquo;</button>`,
    });
 });
+
+
+$('.long_slider').slick({
+    arrows: false,
+    dots: false,
+    centerPadding: '60px',
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
