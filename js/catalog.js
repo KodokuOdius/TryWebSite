1c20d8f4eea536670e59147eeaf7f6e33ba92107
KodokuOdius <alanfoxincoolctranica@gmail.com>
2022-06-20 12:09:49 +0300

    make slider pretty

--- js/catalog.js
+++ js/catalog.js
@@ -1,25 +1,29 @@
 $('.moto_slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+  // centerMode: true,
+  // centerPadding: '60px',
+  infinite: true,
+  // adaptiveHeight: true,
+  slidesToShow: 1,
+  prevArrow: `<button type="button" class="slick-prev">&lsaquo;</button>`,
+  nextArrow: `<button type="button" class="slick-next">&rsaquo;</button>`,
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1
+  //     }
+  //   }
+  // ]
+});
